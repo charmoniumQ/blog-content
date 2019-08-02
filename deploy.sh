6f9f0456481aa768868369181f6489202cecc2f7
Samuel Grayson <sam@samgrayson.me>
2019-08-02 00:56:40 -0700

    generated 2019-56-02 from https://github.com/charmoniumQ/blog-content/commit/3d69baa9fa7689e21ed3fe2594265183857b741a

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,39 @@
 #!/bin/sh
 
+set -e -x
+
 if [ ! -d vendor/bundle ]
 then
     bundle install --path vendor/bundle
 fi
 
-#bundle exec jekyll serve --watch --drafts --future --livereload --incremental --strict_front_matter
-
-bundle exec jekyll build
-
-cd _site
-if [ ! -d .git/ ]
+if [ "$1" = "-i" ]
 then
-    git init
-    git remote add origin git@github.com:charmoniumQ/charmoniumQ.github.io.git
+    bundle exec jekyll serve --watch --drafts --future --livereload --incremental --strict_front_matter
+else
+
+    cd _site
+    if [ ! -d .git/ ]
+    then
+	git init
+	git remote add origin git@github.com:charmoniumQ/charmoniumQ.github.io.git
+	git pull origin master
+    fi
+    cd ..
+
+    bundle exec jekyll build
+
+    commit=$(git rev-parse HEAD)
+
+    cd _site
     touch .nojekyll
+    message="generated $(date +%Y-%M-%d) from https://github.com/charmoniumQ/blog-content/commit/${commit}"
+    git add -A
+    git commit -m "${message}"
+    git push origin master
+    cd ..
 fi
-message=https://github.com/charmoniumQ/blog-content/commit/$(git rev-parse HEAD)
-git commit -am "${message}"
-git push origin master
+
 
 # host=samgrayson.me
 # droppath=/home/sam/tmp/
