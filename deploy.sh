5fc23ebca2b1f023d31a7c11b796455558f8b585
Samuel Grayson <sam@samgrayson.me>
2019-08-03 09:19:59 -0700

    ui tweaks

--- deploy.sh
+++ deploy.sh
@@ -28,6 +28,8 @@ else
     cd _site
     touch .nojekyll
     message="generated $(date +%Y-%M-%d) from https://github.com/charmoniumQ/blog-content/commit/${commit}"
+    echo "${message}"
+    read p
     git add -A
     git commit -m "${message}"
     git push origin master
